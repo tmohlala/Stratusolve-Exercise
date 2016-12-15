c077e2b501718204e148eaba5034a706ca6bdd2b
JohanGriesel <johangriesel@MacBook-Pro-2.local>
2016-12-15 16:13:30 +0200

    Improved task listing

--- list_tasks.php
+++ list_tasks.php
@@ -9,12 +9,22 @@
  * @author     johangriesel <info@stratusolve.com>
  */
 $taskData = file_get_contents('Task_Data.txt');
-if (strlen($taskData) < 1) {
-    $html = '<a id="-1" href="#" class="list-group-item" data-toggle="modal" data-target="#myModal">
+$html = '<a id="-1" href="#" class="list-group-item" data-toggle="modal" data-target="#myModal">
                     <h4 class="list-group-item-heading">No Tasks Available</h4>
                     <p class="list-group-item-text">Click here to create one</p>
                 </a>';
+if (strlen($taskData) < 1) {
     die($html);
 }
-
+$taskArray = json_decode($taskData);
+if (sizeof($taskArray) > 0) {
+    $html = '';
+    foreach ($taskArray as $index=>$task) {
+        $html .= '<a id="'.$task->TaskId.'" href="#" class="list-group-item" data-toggle="modal" data-target="#myModal">
+                    <h4 class="list-group-item-heading">'.$task->TaskName.'</h4>
+                    <p class="list-group-item-text">'.$task->TaskDescription.'</p>
+                </a>';
+    }
+}
+die($html);
 ?>
