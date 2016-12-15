7b425dd90015b26681e2299b8303fa73d52b57c3
JohanGriesel <johangriesel@MacBook-Pro-2.local>
2016-12-15 15:40:28 +0200

    Improved the task class starting point

--- task.class.php
+++ task.class.php
@@ -6,6 +6,7 @@ class Task {
     public $TaskId;
     public $TaskName;
     public $TaskDescription;
+    protected $TaskDataSource;
     public function __construct($Id = null) {
         if ($Id) {
             // This is an existing task
@@ -14,12 +15,17 @@ class Task {
             // This is a new task
             $this->Create();
         }
+        $this->TaskDataSource = file_get_contents('Task_Data.txt');
+        if (strlen($this->TaskDataSource) > 0)
+            $this->TaskDataSource = json_decode($this->TaskDataSource);
+        else
+            $this->TaskDataSource = null;
     }
     protected function Create() {
         // This function needs to generate a new unique ID for the task
         // Assignment: Generate unique id for the new task
-        $this->TaskName = '';
-        $this->TaskDescription = '';
+        $this->TaskName = 'New Task';
+        $this->TaskDescription = 'New Description';
     }
     protected function LoadFromId($Id = null) {
         if ($Id) {
